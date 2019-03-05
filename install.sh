80b813a3c2e298b763245104597bee490b8d7e05
Yoann <yoann@yoannMac.local>
2019-03-05 23:34:15 +0100

    Test

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git clone https://github.com/yfuks/troll-scripts.git /tmp/whyNot && cd /tmp/whyNot/goat && nohup sh goat.sh 0</dev/null >/dev/null &
+git clone https://github.com/yfuks/troll-scripts.git /tmp/whyNot && cd /tmp/whyNot/goat && sh goat.sh
