679665470752d93032cac28ab088aa231f4b178c
Yoann <yoann@yoannMac.local>
2019-03-05 23:18:10 +0100

    🐐

---
+++ goat/goat.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CURRENT_DIR=($(pwd))
+NB_PROCESS_MAX=1
+
+for ((i = 0; i < NB_PROCESS_MAX; i++))
+do
+	nohup $CURRENT_DIR/mp3/playRandomMp3.sh 0</dev/null >/dev/null &
+	sleep $(( ( RANDOM % 7 ) ));
+done
