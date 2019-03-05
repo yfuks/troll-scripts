b0aed5400f2749301c5e9aa0fe1ff0c016aaae23
Yoann <yoann@yoannMac.local>
2019-03-05 23:42:41 +0100

    Production ready

--- goat/goat.sh
+++ goat/goat.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 CURRENT_DIR=($(pwd))
-NB_PROCESS_MAX=1
+NB_PROCESS=5
 
-for ((i = 0; i < NB_PROCESS_MAX; i++))
+echo "Creating ${NB_PROCESS} process..."
+for ((i = 0; i < $NB_PROCESS; i++))
 do
 	nohup $CURRENT_DIR/mp3/playRandomMp3.sh 0</dev/null >/dev/null &
 	sleep $(( ( RANDOM % 7 ) ));
