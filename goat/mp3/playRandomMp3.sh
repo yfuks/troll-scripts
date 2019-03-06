e1c91c28669f92e40825fba7cd408af501faba51
Yoann <yoann@yoannMac.local>
2019-03-06 10:05:22 +0100

    remove MacOS sound

--- goat/mp3/playRandomMp3.sh
+++ goat/mp3/playRandomMp3.sh
@@ -7,7 +7,6 @@ MP3_LIST=($(ls -d $MP3_DIR/*.mp3))
 ## number of mp3
 while true;
 do
-	osascript -e 'set Volume 10'
 	afplay -v 100 ${MP3_LIST[$(( ( RANDOM % ${#MP3_LIST[@]} )))]};
 	sleep $(( ( RANDOM % 7 ) ));
 done
